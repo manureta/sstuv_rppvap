7109dd08249fdb6d59e311f3b0d818fb94b7b7c2
fedesanchez <federico.augusto.sanchez@gmail.com>
2014-11-28 04:30:35 -0300

    ajustando textos de situacion ambiental

--- app/controllers/situacionambiental/SituacionAmbientalEditPanel.class.php
+++ app/controllers/situacionambiental/SituacionAmbientalEditPanel.class.php
@@ -52,18 +52,24 @@ class SituacionAmbientalEditPanel extends SituacionAmbientalEditPanelGen {
             $this->objControlsArray['lstIdFolioObject'] = $this->mctSituacionAmbiental->lstIdFolioObject_Create();
         if (in_array('chkSinProblemas',$strControlsArray)) 
             $this->objControlsArray['chkSinProblemas'] = $this->mctSituacionAmbiental->chkSinProblemas_Create();
+            $this->objControlsArray['chkSinProblemas']->Name="Sin Problemas";
         if (in_array('chkReservaElectroducto',$strControlsArray)) 
             $this->objControlsArray['chkReservaElectroducto'] = $this->mctSituacionAmbiental->chkReservaElectroducto_Create();
+            $this->objControlsArray['chkReservaElectroducto']->Name="Reserva de electroducto";
         if (in_array('chkInundable',$strControlsArray)) 
             $this->objControlsArray['chkInundable'] = $this->mctSituacionAmbiental->chkInundable_Create();
         if (in_array('chkSobreTerraplenFerroviario',$strControlsArray)) 
             $this->objControlsArray['chkSobreTerraplenFerroviario'] = $this->mctSituacionAmbiental->chkSobreTerraplenFerroviario_Create();
+            $this->objControlsArray['chkSobreTerraplenFerroviario']->Name="Sobre terraplén ferroviario";
         if (in_array('chkSobreCaminoSirga',$strControlsArray)) 
             $this->objControlsArray['chkSobreCaminoSirga'] = $this->mctSituacionAmbiental->chkSobreCaminoSirga_Create();
+            $this->objControlsArray['chkSobreCaminoSirga']->Name="Sobre camino Sirga";
         if (in_array('chkExpuestoContaminacionIndustrial',$strControlsArray)) 
             $this->objControlsArray['chkExpuestoContaminacionIndustrial'] = $this->mctSituacionAmbiental->chkExpuestoContaminacionIndustrial_Create();
+            $this->objControlsArray['chkExpuestoContaminacionIndustrial']->Name="Expuesto a contaminación industrial";
         if (in_array('chkSobreSueloDegradado',$strControlsArray)) 
             $this->objControlsArray['chkSobreSueloDegradado'] = $this->mctSituacionAmbiental->chkSobreSueloDegradado_Create();
+            $this->objControlsArray['chkSobreSueloDegradado']->Name="Sobre suelo degradado";
         if (in_array('txtOtro',$strControlsArray)) 
             $this->objControlsArray['txtOtro'] = $this->mctSituacionAmbiental->txtOtro_Create();
 
