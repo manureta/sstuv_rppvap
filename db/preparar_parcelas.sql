b041603ada37f9be0effd50245151273447c97c0
geonode <geonode@Deimos.fibertel.com.ar>
2014-12-04 18:57:46 -0300

    borrando constraint

--- db/preparar_parcelas.sql
+++ db/preparar_parcelas.sql
@@ -1,2 +1,3 @@
+alter table parcelas drop constraint enforce_srid_geom;
 update parcelas set geom=st_transform(geom,4326);
 create view v_folios as select id as gid,st_geomfromtext(geom,4326) as the_geom from folio;
