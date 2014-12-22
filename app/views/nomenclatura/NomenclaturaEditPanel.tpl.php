96b3902175fd11610c323ac8d135f38b654632a3
fedesanchez <federico.augusto.sanchez@gmail.com>
2014-12-22 00:19:14 -0300

    reordenando variables de nmenclatura, saco uso interno

--- app/views/nomenclatura/NomenclaturaEditPanel.tpl.php
+++ app/views/nomenclatura/NomenclaturaEditPanel.tpl.php
@@ -1,8 +1,7 @@
 
 		
 		<?php $_CONTROL->lstIdFolioObject->RenderWithName(); ?>
-		<?php $_CONTROL->txtPartidaInmobiliaria->RenderWithName(); ?>
-		<?php $_CONTROL->txtTitularDominio->RenderWithName(); ?>
+		
 		<?php $_CONTROL->txtPartido->RenderWithName(); ?>
 		<?php $_CONTROL->txtCirc->RenderWithName(); ?>
 		<?php $_CONTROL->txtSecc->RenderWithName(); ?>
@@ -10,15 +9,8 @@
 		<?php $_CONTROL->txtFrac->RenderWithName(); ?>
 		<?php $_CONTROL->txtMza->RenderWithName(); ?>
 		<?php $_CONTROL->txtParc->RenderWithName(); ?>
-		
-	<div id="steps-tabs-folio" role="application" class="wizard clearfix">
-
-		<div class="titulos"><i class="icon-chevron-right"> Uso Interno</i></div> 
-		
-			<div class="well bs-component">
-				<?php $_CONTROL->txtInscripcionDominio->RenderWithName(); ?>
-			
-				<?php $_CONTROL->chkDatoVerificadoRegPropiedad->RenderWithName(); ?>
-			</div>
-			
-	</div>	
+		<?php $_CONTROL->txtPartidaInmobiliaria->RenderWithName(); ?>
+		<?php $_CONTROL->txtTitularDominio->RenderWithName(); ?>
+		<?php $_CONTROL->txtInscripcionDominio->RenderWithName(); ?>
+		<?php $_CONTROL->chkDatoVerificadoRegPropiedad->RenderWithName(); ?>
+	
