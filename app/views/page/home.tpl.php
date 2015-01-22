0c122954d0e657a9c99b6f9e93f2c506cc6e4a40
fedesanchez <federico.augusto.sanchez@gmail.com>
2015-01-22 03:54:35 -0300

    ordenando texto de home

--- app/views/page/home.tpl.php
+++ app/views/page/home.tpl.php
@@ -1,9 +1,14 @@
 
-<p>Bienvenido al Registro Público Provincial de Villas y Asentamientos Precarios!</p>
+<address>
+  <strong>Bienvenido al Registro Público Provincial de Villas y Asentamientos Precarios!</strong><br>
+  Este Registro constituye una herramienta única y centralizada de relevamiento, captación y administración de datos <br>
+  para la implementación del régimen de integración socio-urbana de villas y asentamiento precarios en el marco de la ley 14449.<br>
+  Para comenzar la carga de la información catastral, dominial, social y urbanística de un barrio haga clic <a href="/registro/folio">aquí</a> o en el botón “Folios”. <br>
 
-<p>Este Registro constituye una herramienta única y centralizada de relevamiento, captación y administración de datos para la implementación del régimen de integración socio-urbana de villas y asentamiento precarios en el marco de la ley 14449.</p>
+</address>
 
-<p>Para comenzar la carga de la información catastral, dominial, social y urbanística de un barrio haga clic aquí o en el botón “Folios”. Le sugerimos consulte el instructivo de carga y ante cualquier duda o dificultad no deje de escribirnos a registrovillasyasentamientos@gmail.com</p>
-
-<p>Muchas gracias por su colaboración.</p>
-
+<address>
+	 Le sugerimos consulte el instructivo de carga y ante cualquier duda o dificultad no deje de escribirnos a 	
+  <a href="mailto:#">registrovillasyasentamientos@gmail.com</a><br>
+  <strong>Muchas gracias por su colaboración.</strong>
+</address>
