7e5a74de2e4b856973ad6a7f92797f7c45cf4853
Fede Sanchez <fedesanchez888@gmail.com>
2015-11-28 00:58:11 -0300

    texto inicial

--- app/views/page/home.tpl.php
+++ app/views/page/home.tpl.php
@@ -1,31 +1,37 @@
 
-<address>
-  <strong>Bienvenido al Registro Público Provincial de Villas y Asentamientos !</strong><br>
-Este Registro constituye una herramienta única y centralizada de relevamiento, captación 
-
-y administración de datos para la implementación del régimen de integración socio-
-
-urbana de villas y asentamiento precarios en el marco de la Ley 14.449.
-<br />
-Para comenzar la carga de la información catastral, dominial, social y urbanística de un 
-
-barrio haga clic <a href="<?php echo __VIRTUAL_DIRECTORY__;?>/folio">aquí</a> o en el botón “Folios”. 
-</address>
-
-<address>
-	 Le sugerimos consulte el <a target="_blank" href="http://www.sstuv.tk/sig/wp-content/uploads/2015/01/Instructivo-de-uso-Aplicativo_Municipios.pdf">instructivo de carga</a> y ante cualquier 
-
-duda o dificultad no deje<br /> de escribirnos a	<a href="mailto:#">registrovillasyasentamientos@gmail.com</a><br>
-<br>
 <p>
-	<strong>Muchas gracias por su colaboración.</strong>
-</p>	
-</address>
+  <strong>Bienvenido al Registro Público Provincial de Villas y Asentamientos !</strong>
+</p>
+<p>
+  Esta aplicación constituye una herramienta que integra requerimientos de tres Direcciones Provinciales de la Subsecretaría Social de Tierra Urbanismo y Vivienda, permitiendo el seguimiento y la administración de manera centralizada de la información de los barrios que componen el Registro Público Provincial de Villas y Asentamientos Precarios (Ley 14.449), las gestiones realizadas en el marco del Área de Conflictos Habitacionales con el objetivo de suspender los desalojos y las intervenciones realizadas por la Dirección de Regularización Urbana y Dominial.
+</p>
+<p>
+El sistema funciona con distintos perfiles de usuarios a los con distintos permisos de visualización o edición de la información y se compone básicamente de una guía de barrio que se articula con un sistema de información geográfica, que facilita la identificación catastral y dominial de las parcelas que componen cada barrio. A su vez, cada uno de los barrios posee un identificador único que es un número de Folio (equivalente al DNI de una persona). Por medio de este número no sólo es posible disponer de un repositorio de información centralizada para almacenar fotos, planos, informes, resoluciones y cualquier otro tipo documentación, sino acceder siete solapas temáticas para cada barrio:
+</p>
+
+<p>
+  <ul>
+    <li> Datos Generales del Barrio </li>
+
+    <li> Nomenclatura Catastral y Dominio</li>
+
+    <li> Condiciones Socio-Urbanísticas</li>
+
+    <li> Regularización e Integración socio-urbana</li>
+
+    <li> Conflictos y Judicialización</li>
+
+    <li> Censo y Adjudicación</li>
+
+    <li> Comentarios</li>
+  </ul>
+</p>
+
+<p>
+  Ésta última funcionalidad (Comentarios) se encuentra orientada a facilitar la comunicación entre las distintas áreas de la Subsecretaría acerca del seguimiento de la acciones en un mismo barrio y, al mismo tiempo, permitir documentar todo aquello que el sistema no habilita a través del modelo de datos estructurado.
+</p>
+
 
 <address>
-	<strong>ACLARACION:</strong> EN LA ACTUALIDAD NOS ENCONTRAMOS EN PROCESO DE RECOPILACIÓN 
-	E IDENTIFICACIÓN DE LOS BARRIOS QUE CONFORMARÁN EL REGISTRO. <br />
-    POR ELLO RECOMENDAMOS TENER EN CUENTA QUE LA INFORMACIÓN DISPONIBLE 
-	MUESTRA DIFERENTES GRADOS DE AVANCES POR MUNICIPIOS. EN OCASIONES EL 
-    UNIVERSO DE BARRIOS PUEDE SER PARCIAL O AÚN NO ESTAR VALIDADOS EN SU TOTALIDAD.
+Para cualquier duda o consulta al (0221) 427-2266 Interno 157
 </address>
