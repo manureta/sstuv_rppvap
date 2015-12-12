a830deed8a008b28c272870d25b39d2074b3f96e
Fede Sanchez <fedesanchez888@gmail.com>
2015-12-12 01:21:42 -0300

    acomodo el panel de usuarios y pongo usuarios

--- app/helpers/UserMenuPanel.class.php
+++ app/helpers/UserMenuPanel.class.php
@@ -6,7 +6,7 @@ class UserMenuPanel extends QPanel {
     public $pnlNotificaciones;
     public $pnlMensajes;
     public $pnlUsuario;
-    
+
     public function __construct($objParentObject, $strSetEditPanelMethod = null, $strCloseEditPanelMethod = null, $strControlId = null) {
         // Call the Parent
 
@@ -18,7 +18,7 @@ class UserMenuPanel extends QPanel {
         }
 //        $this->Template = __PROJECT_DIR__ . '/public/tmp/menu-usuario.html';
         $this->blnAutoRenderChildren = true;
-        
+
         $btnInicio = new QLinkButton($this);
         $btnInicio->Text = 'Inicio';
         $btnInicio->Icon = 'home';
@@ -31,7 +31,7 @@ class UserMenuPanel extends QPanel {
         $btnMapa->AddCssClass('navbar-link');
         //$btnMapa->AddAction(new QClickEvent(),  new QAjaxControlAction($this, 'btnMapa_Click'));
         $btnMapa->Link = "/mapa/";
-        
+
         $btnFolio = new QLinkButton($this);
         $btnFolio->Text = 'Folios';
         $btnFolio->Icon = 'edit';
@@ -45,21 +45,29 @@ class UserMenuPanel extends QPanel {
          $btnReportes->AddCssClass('navbar-link');
          $btnReportes->AddAction(new QClickEvent(),  new QAjaxControlAction($this, 'btnReportes_Click'));
         }
-        
+
         if(Permission::PuedeConsultarTripartito()){
          $btnTripartito = new QLinkButton($this);
          $btnTripartito->Text = 'Tripartito';
-         $btnTripartito->Icon = 'download';
+         $btnTripartito->Icon = 'search';
          $btnTripartito->AddCssClass('navbar-link');
          $btnTripartito->AddAction(new QClickEvent(),  new QAjaxControlAction($this, 'btnTripartito_Click'));
         }
-   
+
+        if(Permission::EsSuperAdministrador()){
+         $btnUsuarios = new QLinkButton($this);
+         $btnUsuarios->Text = 'Usuarios';
+         $btnUsuarios->Icon = 'user';
+         $btnUsuarios->AddCssClass('navbar-link');
+         $btnUsuarios->AddAction(new QClickEvent(),  new QAjaxControlAction($this, 'btnUsuarios_Click'));
+        }
+
         $btnCerrarSesion = new QLinkButton($this);
         $btnCerrarSesion->Text = 'Salir';
         $btnCerrarSesion->Icon = 'off';
         $btnCerrarSesion->AddCssClass('navbar-link');
         $btnCerrarSesion->AddAction(new QClickEvent(),  new QAjaxControlAction($this, 'btnCerrarSesion_Click'));
-   
+
     }
 
     public function btnHome_Click() {
@@ -70,32 +78,20 @@ class UserMenuPanel extends QPanel {
         Session::End();
         QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/');
     }
-    
+
     public function btnEditarPerfil_Click() {
         QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/usuario/perfil');
     }
-    
-    
+
+
 
     public function btnFolio_Click() {
         QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/folio');
     }
-    
-    
-/*
-    public function btnCedula_Click() {
-        //QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/cedula');
-        $arrInfoUsuario = Session::GetUsuario();
-        $objPersonalEstablecimientoUnidadServicioArray = PersonalEstablecimientoUnidadServicio::LoadArrayByIdPersonal($arrInfoUsuario['IdPersonal']);
-        if (count($objPersonalEstablecimientoUnidadServicioArray) > 0) {
-            QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/folio');
-        } else {
-            QApplication::DisplayAlert('Usted no posee Ofertas para cargar');
-        }
-    }
-*/    
-    
-    
+
+
+
+
     public function lnkRedirect_Click($strFormId, $strControlId, $strParameter) {
         QApplication::Redirect($strParameter);
     }
@@ -111,5 +107,9 @@ class UserMenuPanel extends QPanel {
 
     public function btnTripartito_Click() {
         QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/page/tripartito');
+    }
+
+    public function btnUsuarios_Click() {
+        QApplication::Redirect(__VIRTUAL_DIRECTORY__.'/usuario');
     }
 }
